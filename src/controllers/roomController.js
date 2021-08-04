6afe1d0a445710a25749292c12f7769ee707d9fa
Otávio Alves Rosa <008028@colegiopassoseguro.com.br>
2021-08-03 22:03:57 -0300

    db management feature; password verification; enter room feature;

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -5,19 +5,55 @@ module.exports = {
     db = await Database()
     const password = req.body.password
     let roomId = ''
-    for (let i = 0; i < 6; i++) {
-      roomId += Math.floor(Math.random() * 10).toString()
+    let tryRoom = true
+
+    while (tryRoom) {
+      for (let i = 0; i < 6; i++) {
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
+      const dbRooms = await db.all(`SELECT id FROM rooms`)
+      tryRoom = dbRooms.some(rooms => rooms === roomId)
+      if (!tryRoom) {
+        await db.run(`INSERT INTO rooms (
+          id,
+          password
+          ) VALUES (
+          ${parseInt(roomId)},
+          "${password}"
+          )`)
+      }
     }
 
-    await db.run(`INSERT INTO rooms (
-      id,
-      password
-      ) VAlUES (
-      ${parseInt(roomId)},
-      ${password}
-      )`)
-
     await db.close()
+    res.redirect(`/room/${roomId}`)
+  },
+
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId} AND read = 0`
+    )
+    const questionsRead = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId} AND read = 1`
+    )
+    let isQuestions = true
+    if (questions.length == 0) {
+      if (questionsRead.length == 0) {
+        isQuestions = false
+      }
+    }
+
+    res.render('room', {
+      roomId: roomId,
+      questions: questions,
+      questionsRead: questionsRead,
+      isQuestions: isQuestions
+    })
+  },
+
+  enter(req, res) {
+    roomId = req.body.roomId
     res.redirect(`/room/${roomId}`)
   }
 }
--- src/route.js
+++ src/route.js
@@ -8,8 +8,13 @@ route.get('/create-room', (req, res) =>
   res.render('index', { page: 'create-room' })
 )
 
-route.get('/room/:room', (req, res) => res.render('room'))
+route.get('/room/:room', roomController.open)
 
-route.post('/question/:code/:question/:action', questionController.index)
 route.post('/new-room', roomController.create)
+
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:code/:question/:action', questionController.index)
+
+route.post('/join-room', roomController.enter)
+
 module.exports = route
